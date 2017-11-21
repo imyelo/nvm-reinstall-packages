84ed5cccdf64419587351c831ca04a866ec99faa
yelo <zhihuzeye@gmail.com>
2017-11-21 20:09:19 +0800

    init repo

---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require('fs')
+const path = require('path')
+const flatten = require('lodash.flatten')
+const meow = require('meow')
+const execa = require('execa')
+const chalk = require('chalk')
+
+const NVM_DIR = process.env.NVM_DIR
+
+function main () {
+  const cli = meow(`
+    Usage
+      $ nvm-reinstall-packages <from-version>
+
+    Options
+      --list  Lists packages only, without installation
+
+    Examples
+      $ nvm-reinstall-packages 7.10.1
+  `, {
+    flats: {
+      list: {
+        type: 'boolean',
+      },
+    },
+  })
+
+  const version = cli.input[0]
+  if (!version) {
+    throw new Error('<from-version> is required.')
+  }
+
+  const home = node_modules(version)
+
+  if (!fs.existsSync(home)) {
+    throw new Error(`version ${version} not found.`)
+  }
+
+  const pkgs = packages(home)
+
+  if (cli.flags.list) {
+    console.log(pkgs.join(' '))
+    return
+  }
+
+  install(pkgs)
+}
+
+function node_modules (version) {
+  return `${NVM_DIR}/versions/node/v${version}/lib/node_modules`
+}
+
+function packages (node_modules) {
+  const dirs = fs.readdirSync(node_modules)
+
+  return flatten([
+    flatten(
+      dirs
+        .filter((dir) => dir.indexOf('@') === 0)
+        .map((namespace) => {
+          return fs.readdirSync(path.join(node_modules, namespace)).map((dir) => `${namespace}/${dir}`)
+        })
+    ),
+    dirs
+      .filter((dir) => dir.indexOf('@') !== 0),
+  ])
+}
+
+function install (packages) {
+  console.log([
+    '',
+    chalk.bgBlue('These packages will be installed by npm:'),
+    chalk.gray(packages.join(', ')),
+    '',
+  ].join('\n'))
+
+  const child = execa('npm', ['install', '--global', ...packages])
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+  return child
+}
+
+main()
